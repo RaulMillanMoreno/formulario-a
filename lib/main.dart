468367b7dea9ad785b069af7ab81d66eb361b331
RaulMillanMoreno <raul.millan@sarria.salesians.cat>
2024-11-04 11:40:07 +0100

    pre_propuesta
    antes de hacer las modificaciones que el profesor a entregado

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
-
-
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
@@ -11,11 +9,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Recuperar el valor d''un camp de text',
-      theme: ThemeData(       
+      title: 'Recuperar el valor d\'un camp de text',
+      theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
-      ),      
+      ),
       home: MyCustomForm(title: 'por fin tio'),
     );
   }
@@ -31,6 +29,7 @@ class MyCustomForm extends StatefulWidget {
 
 class _MyCustomFormState extends State<MyCustomForm> {
   final myController = TextEditingController();
+  final _formKey = GlobalKey<FormBuilderState>();
   void _onChanged(dynamic val) => debugPrint(val.toString());
 
   @override
@@ -44,79 +43,215 @@ class _MyCustomFormState extends State<MyCustomForm> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title:Text(widget.title),
+        title: Text(widget.title),
       ),
-      body: Container(// en esta parte es donde se pone el texto del inicio     
-        child: Column(
-          children: <Widget>[
-            Text(// estos son los primeros textos y es donde se ponen sus especificaciones.
-            "Driving Form",
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 55,
-                fontWeight: FontWeight.bold,
-                fontStyle: FontStyle.normal
-              ),
-              textAlign: TextAlign.center,
+      body: SingleChildScrollView(
+
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: FormBuilder(
+            key: _formKey, 
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[// en esta parte es donde se pone el texto del inicio
+                
+                Center(
+                  child: Text(
+                     "Driving Form",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 55,
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.normal,
+                    ),
+                  ),
+
+                ),
+                
+                Text(
+                  "Driving Form",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 55,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.normal,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                
+                
+                Text(
+                  "Form examples",
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.normal,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 20),
+                Text( // estos son los primeros textos y es donde se ponen sus especificaciones.
+                  "Form examples ",
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.normal,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                Text(
+                  "Form examples ",
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.normal,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 20),
+
+                FormBuilderRadioGroup<String>(
+                  initialValue: null,
+                  name: 'best_language',
+                  onChanged: _onChanged,
+                  validator: FormBuilderValidators.compose(
+                      [FormBuilderValidators.required()]),
+                  options: ['Dart','Kotlin','Java','Swift','Objective-C']
+                      .map((lang) => FormBuilderFieldOption(
+                            value: lang,
+                            child: Row( // Cambiamos a Row para controlar la disposición
+                              children: [
+                                Text(lang),
+                              ],
+                            ),
+                          ))
+                      .toList(growable: false),
+                  controlAffinity: ControlAffinity.leading, // Cambia a trailing si prefieres que el texto esté a la derecha
+                ),
+                const SizedBox(height: 20),
+
+                Text(
+                  "Enter remarks",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.normal,
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Enter your remarks', // Texto de sugerencia dentro del campo
+                    filled: true, // Para llenar el fondo del campo
+                    fillColor: const Color.fromARGB(255, 228, 222, 222), // Color de fondo similar al de la imagen
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0), // Bordes redondeados
+                      borderSide: BorderSide(// no sale el color del borde
+                        color: Colors.red, // Color del borde
+                        width: 1.0, // Grosor del borde
+                      ),
+                    ),
+                  ),
+                ),
+
+                Text(
+                  "Enter remarks",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.normal,
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+
+                Text(
+                  "Enter remarks",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.normal,
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+
+                  
+                const SizedBox(height: 20),
+
+                // Campo de texto adicional
+                FormBuilderTextField(
+                  name: 'remarks',
+                  decoration: InputDecoration(
+                    hintText: 'Enter your remarks',
+                    filled: true,
+                    fillColor: const Color.fromARGB(255, 228, 222, 222),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                      borderSide: const BorderSide(color: Colors.red),
+                    ),
+                  ),
+                ),
+
+                // ComboBox
+                FormBuilderDropdown<String>(
+                  name: 'option_s',
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    filled: true,
+                    fillColor: Colors.transparent,
+                  ),
+                  hint: const Text('Select option'),
+                  validator: FormBuilderValidators.compose(
+                    [FormBuilderValidators.required()],
+                  ),
+                  items: ['Dart', 'Kotlin', 'Java', 'Swift', 'Objective-C']
+                      .map((lang) => DropdownMenuItem(
+                            value: lang,
+                            child: Text(lang),
+                          ))
+                      .toList(),
+                  onChanged: _onChanged,
+                ),
+
+                Text(
+                  "Enter remarks",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.normal,
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+
+                Text(
+                  "Enter remarks",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.normal,
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+              ],
             ),
-
-            Text(// estos son los primeros textos y es donde se ponen sus especificaciones.
-            "Form examples",
-              style: TextStyle(
-                color: Colors.grey,
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-                fontStyle: FontStyle.normal
-              ),
-              textAlign: TextAlign.center,              
-            ),
-            const SizedBox(height: 20),
-
-            Text(// estos son los primeros textos y es donde se ponen sus especificaciones.
-            "Form examples ",
-              style: TextStyle(
-                color: Colors.grey,
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-                fontStyle: FontStyle.normal
-              ),
-              textAlign: TextAlign.center,
-            ),
-            
-            Text(// estos son los primeros textos y es donde se ponen sus especificaciones.
-            "Form examples ",
-              style: TextStyle(
-                color: Colors.grey,
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-                fontStyle: FontStyle.normal
-              ),
-              textAlign: TextAlign.center,
-            ),
-            
-            FormBuilderRadioGroup<String>(
-              initialValue: null,
-              name: 'best_language',
-              onChanged: _onChanged,
-              validator: FormBuilderValidators.compose(
-                [FormBuilderValidators.required()]),
-              options: ['Dart', 'Kotlin', 'Java', 'Swift', 'Objective-C']
-                  .map((lang) => FormBuilderFieldOption(
-                        value: lang,
-                        child: Text(lang),
-                      ))
-                  .toList(growable: false),
-              controlAffinity: ControlAffinity.leading,
-            ),
-
-
-
-
-            
-          ],
-        )       
-      
+          )
+        )
       ),
+
+
+
+
+
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           showModalBottomSheet(
@@ -152,5 +287,4 @@ class _MyCustomFormState extends State<MyCustomForm> {
       ),
     );
   }
-  
 }
